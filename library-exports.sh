a4b05ded322ac8db4832b9263a8b82711955d418
Stephen Fox <stephen.j.fox.jr@gmail.com>
2024-07-07 14:43:07 -0400

    library-exports: Initial commit.

---
+++ library-exports.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -eu
+
+which gojq > /dev/null
+which rizin > /dev/null
+
+library_file="${1}"
+cmd=''
+
+cmd="$(rz-bin -Ej "${library_file}" \
+  | gojq -r '.exports.[] | "\(.name) \(.vaddr)"' \
+  | {
+  while read l
+  do
+    name=${l% *}
+    offset=${l##* }
+    cmd="${cmd}s ${offset}; afs;"
+  done
+  echo "${cmd}"
+})"
+
+rizin -AA -c "${cmd}" -Q "${library_file}"

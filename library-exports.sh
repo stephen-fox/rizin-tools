2d9cc82f81723803e1f36e8c6d5b4fdbe14af9a6
Stephen Fox <stephen.j.fox.jr@gmail.com>
2024-08-17 15:34:22 -0400

    library-exports.sh: Added some comments.

--- library-exports.sh
+++ library-exports.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# library-exports.sh gets the function signatures for a library's
+# exported symbols:
+#
+# usage: library-exports.sh /path/to/library
+
 set -eu
 
 which gojq > /dev/null
@@ -8,6 +13,10 @@ which rizin > /dev/null
 library_file="${1}"
 cmd=''
 
+# Get exported symbols from library and build list of addresses to seek to
+# and subsequently get function signature.
+#
+# E.g., "s 268440336; afs;s 268440704; afs;s 268441392".
 cmd="$(rz-bin -Ej "${library_file}" \
   | gojq -r '.exports.[] | "\(.name) \(.vaddr)"' \
   | {
